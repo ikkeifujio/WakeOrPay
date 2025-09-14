10244df210b2d1051bc3b82dc202c7a1acfb2ec5
ikkeifujio <fjo_1k.appbank@icloud.com>
2025-09-14 15:18:04 +0900

    Initial Commit

---
+++ WakeOrPay/WakeOrPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeOrPayApp.swift
+//  WakeOrPay
+//
+//  Created by fujio ikkei on 2025/09/14.
+//
+
+import SwiftUI
+
+@main
+struct WakeOrPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
